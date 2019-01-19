4fe5d226f0bfcfbc40c889a418571444463a79d7
AmDji <adamghannoum98@gmail.com>
2019-01-19 05:54:08 -0500

    Added the ability to create products using post requests using browser tools

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,8 +1,25 @@
 class Api::V1::ProductsController < ApplicationController
 
-  def index #/api/v1/products
+  def index #/api/v1/products (fetches all products at once)
     @products = Product.all
     render( {json: @products, status: :ok} )
+  end
+
+  def show #/api/v1/products/id (fetches 1 product via id ~ id should be replaced by a number)
+    @Product = Product.find(params[:id])
+    render json: @Product, status: :ok
+  end
+
+  def create # POST api/v1/products
+    @product = Product.create(product_params)
+    render json: @product, status: :created
+  end
+
+  private
+
+  #Whitelisting the appropriate attributes
+  def product_params
+    params.require(:product).permit(:title, :price, :inventory_count, :availability)
   end
 
 end
